f7d5d91a8b1841417febb460370d5274c7bad752
eflaatten <elliotflaatten@live.com>
2024-03-10 16:21:48 -0500

    99% done

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,42 @@
-scrollToTop = () => {
-  const aboutContainer = document.querySelector('.about-me')
-  aboutContainer.scrollIntoView({behavior: "smooth"})
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 
-responsiveNav = () => {
-  var x = document.getElementById("nav-bar");
-  if (x.className === "nav-container") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-bar";
-  }
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
 
-window.addEventListener('scroll', function() {
-  let h2 = document.querySelector('.about-me-container h2');
-  let p = document.querySelector('.about-me-container p');
-  let scrollPosition = window.scrollY;
 
-  // Adjust the position of h2 and p elements based on the scroll position
-  h2.style.transform = `translateY(-${scrollPosition * 0.3}px)`;
-  p.style.transform = `translateY(-${scrollPosition * 0.7}px)`; // Adjust the speed as needed
-});
+// Get the scroll button element
+var scrollButton = document.querySelector(".scroll-up");
 
-
-
-let lastScrollTop = 0;
-window.addEventListener('scroll', function() {
-  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-  // Hide/show nav bar when scrolling
-  let navBar = document.querySelector('nav');
-  if (currentScroll > lastScrollTop) {
-    // Downscroll code
-    navBar.classList.add("hide");
-  } else {
-    // Upscroll code
-    navBar.classList.remove("hide");
+// Add scroll event listener to window
+window.addEventListener("scroll", function() {
+  // If user scrolled down, show the scroll button
+  if (window.scrollY > 0) {
+    scrollButton.style.display = "block";
+  } else { // Otherwise, hide it
+    scrollButton.style.display = "none";
   }
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
 });
+
+// Function to scroll to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
 
